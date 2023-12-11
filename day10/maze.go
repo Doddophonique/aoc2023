5bebd71c4e7137108c6d7b2f6b26380c6db7a80a
Davide Oddone <davide@oddone.dev>
2023-12-11 22:32:49 +0100

    Day10 skeleton

---
+++ day10/maze.go
@@ -0,0 +1,42 @@
+package main
+
+import()
+
+// Parallel code, global vars
+type Nodes struct {
+	mu       	sync.Mutex
+	variable	type
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func PrintAndWait(x ...any) {
+	fmt.Print(x...)
+	fmt.Scanln()
+}
+// use defer timer("funcname")() when the function you want to
+// test starts
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
+func main() {
+	file, err := os.Open("./inputs")
+	check(err)
+	defer file.Close()
+	
+	lines := make([]string, 0)
+	
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	    lines = append(lines, scanner.Text())
+	}
+	        
+}   	
